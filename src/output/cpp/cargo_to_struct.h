7ce171712e757531383f15d97877348484c05393
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-11-05 11:26:34 +0900

    add for fixed array

--- src/output/cpp/cargo_to_struct.h
+++ src/output/cpp/cargo_to_struct.h
@@ -8,6 +8,80 @@
 
 namespace binred {
     struct CargoToCppStruct {
+        static ExprLength* get_exprlength(std::shared_ptr<Param>& param) {
+            auto bin = static_cast<Builtin*>(&*param);
+            return static_cast<ExprLength*>(&*bin->length);
+        }
+
+        static bool get_typename(std::string& tyname, OutContext& ctx, std::shared_ptr<Param>& param, size_t& bylen) {
+            auto type = param->type;
+            if (type == ParamType::integer || type == ParamType::uint || type == ParamType::bit) {
+                auto lenp = get_exprlength(param);
+                auto res = get_const_int<size_t>(lenp->expr);
+                if (!res.second) {
+                    return false;
+                }
+                if (type == ParamType::integer || type == ParamType::uint) {
+                    tyname = "std::";
+                    if (type == ParamType::uint) {
+                        tyname += "u";
+                    }
+                    switch (res.first) {
+                        case 1:
+                            tyname += "int8_t";
+                            break;
+                        case 2:
+                            tyname += "int16_t";
+                            break;
+                        case 3:
+                        case 4:
+                            tyname += "int32_t";
+                            break;
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                            tyname += "int64_t";
+                            break;
+                        default:
+                            return false;
+                    }
+                }
+                else {
+                    size_t base = res.first;
+                    if (base <= 8) {
+                        tyname += "std::uint8_t";
+                    }
+                    else if (base <= 16) {
+                        tyname += "std::uint16_t";
+                    }
+                    else if (base <= 32) {
+                        tyname += "std::uint32_t";
+                    }
+                    else if (base <= 64) {
+                        tyname += "std::uint64_t";
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+            else if (type == ParamType::byte) {
+                auto lenp = get_exprlength(param);
+                if (auto got = get_const_int<size_t>(lenp->expr); got.second && got.first != 0) {
+                    tyname = "std::uint8_t";
+                    bylen = got.first;
+                }
+                else {
+                    tyname += ctx.buffer_type();
+                }
+            }
+            else {
+                return false;
+            }
+            return true;
+        }
+
         static bool convert(OutContext& ctx, Cargo& cargo) {
             ctx.write("\nstruct ");
             ctx.write(cargo.name);
@@ -15,63 +89,8 @@ namespace binred {
             for (auto i = 0; i < cargo.params.size(); i++) {
                 std::string tyname;
                 auto& param = cargo.params[i];
-                auto type = param->type;
-                if (type == ParamType::integer || type == ParamType::uint || type == ParamType::bit) {
-                    auto bin = static_cast<Builtin*>(&*param);
-                    auto lenp = static_cast<ExprLength*>(&*bin->length);
-                    auto res = get_const_int<size_t>(lenp->expr);
-                    if (!res.second) {
-                        return false;
-                    }
-                    if (type == ParamType::integer || type == ParamType::uint) {
-                        tyname = "std::";
-                        if (type == ParamType::uint) {
-                            tyname += "u";
-                        }
-                        switch (res.first) {
-                            case 1:
-                                tyname += "int8_t";
-                                break;
-                            case 2:
-                                tyname += "int16_t";
-                                break;
-                            case 3:
-                            case 4:
-                                tyname += "int32_t";
-                                break;
-                            case 5:
-                            case 6:
-                            case 7:
-                            case 8:
-                                tyname += "int64_t";
-                                break;
-                            default:
-                                return false;
-                        }
-                    }
-                    else {
-                        size_t base = res.first;
-                        if (base <= 8) {
-                            tyname += "std::uint8_t";
-                        }
-                        else if (base <= 16) {
-                            tyname += "std::uint16_t";
-                        }
-                        else if (base <= 32) {
-                            tyname += "std::uint32_t";
-                        }
-                        else if (base <= 64) {
-                            tyname += "std::uint64_t";
-                        }
-                        else {
-                            return false;
-                        }
-                    }
-                }
-                else if (type == ParamType::byte) {
-                    tyname += ctx.buffer_type();
-                }
-                else {
+                size_t bylen = 0;
+                if (!get_typename(tyname, ctx, param, bylen)) {
                     return false;
                 }
                 auto& name = param->name;
@@ -79,9 +98,20 @@ namespace binred {
                 ctx.write(tyname);
                 ctx.write(" ");
                 ctx.write(name);
+                if (bylen != 0) {
+                    ctx.write("[");
+                    ctx.write(std::to_string(bylen));
+                    ctx.write("]");
+                }
                 ctx.write(";\n\n");
                 ctx.write("public:\n");
+                if (bylen != 0) {
+                    ctx.write("const ");
+                }
                 ctx.write(tyname);
+                if (bylen != 0) {
+                    ctx.write("*");
+                }
                 ctx.write(" get_");
                 ctx.write(name);
                 ctx.write("() const {\nreturn ");
@@ -92,10 +122,15 @@ namespace binred {
                 ctx.write(name);
                 ctx.write("(const ");
                 ctx.write(tyname);
-                ctx.write("& __v_input");
+                if (bylen != 0) {
+                    ctx.write("* __v_input");
+                }
+                else {
+                    ctx.write("& __v_input");
+                }
                 ctx.write(") {\n");
                 std::string err = cargo.name + "_" + name;
-                if (type == ParamType::byte) {
+                if (param->type == ParamType::byte && bylen == 0) {
                     ctx.set_error_enum(err + "_length");
                     ctx.write("if ((");
                     ctx.write(ctx.length_of_byte("__v_input"));
@@ -130,9 +165,21 @@ namespace binred {
                     ctx.write("::" + err + "_if");
                     ctx.write(";\n}\n");
                 }
-                ctx.write("this->");
-                ctx.write(name);
-                ctx.write("=__v_input;\n");
+                if (bylen != 0) {
+                    ctx.write("::memcpy(this->");
+                    ctx.write(name);
+                    ctx.write(",__v_input,");
+                    ctx.write(std::to_string(bylen));
+                    ctx.write(");\n");
+                }
+                else {
+                    ctx.write("this->");
+                    ctx.write(name);
+                    ctx.write("=__v_input;\n");
+                }
+                ctx.write("return ");
+                ctx.write(ctx.error_enum());
+                ctx.write("::none;\n");
                 ctx.write("}\n");
             }
             ctx.write("};\n");
