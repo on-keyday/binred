f4ca927bb8d8b5991b7593f92c6ea9136733929c
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-11-05 13:11:51 +0900

    add block load

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,17 @@
 #include <thread>
 using Recv = commonlib2::RecvChan<int>;
 void test_thread(Recv r) {
+    while (true) {
+        int data = 0;
+        if (auto e = r >> data) {
+            std::cout << data << "\n";
+            Sleep(1);
+        }
+        else if (e == commonlib2::ChanError::closed) {
+            std::cout << "closed\n";
+            break;
+        }
+    }
 }
 
 int main(int argc, char** argv) {
@@ -21,11 +32,23 @@ int main(int argc, char** argv) {
     binred::OutContext ctx;
     auto c = static_cast<binred::Cargo*>(&*result[0]);
     binred::CargoToCppStruct::convert(ctx, *c);
-    std::ofstream fs("D:/MiniTools/binred/generated/test.hpp");
-    std::cout << ctx.buffer;
-    fs << "/*license*/\n#pragma once\n#include<cstdint>\n#include<string>\n";
-    fs << binred::error_enum_class(ctx);
-    fs << ctx.buffer;
+    {
+        std::ofstream fs("D:/MiniTools/binred/generated/test.hpp");
+        std::cout << ctx.buffer;
+        fs << "/*license*/\n#pragma once\n#include<cstdint>\n#include<string>\n";
+        fs << binred::error_enum_class(ctx);
+        fs << ctx.buffer;
+    }
     auto [w, r] = commonlib2::make_chan<int>();
-    std::thread(test_thread, r).detach();
+    std::thread(test_thread, std::move(r)).detach();
+    size_t count = 0;
+    while (w << count) {
+        if (count >= 1000) {
+            w.close();
+            break;
+        }
+        Sleep(1);
+        count++;
+    }
+    Sleep(10);
 }
